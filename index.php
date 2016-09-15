8722b4cfe6a2067fdf49f667ff395ca66608a8eb
Satmage <kleckers@yandex.ru>
2016-09-15 14:26:42 +0300

    Правки путей

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-require __DIR__ . '/../classes/DB.php';
-require_once __DIR__ . '/../classes/var_dump.php';
-require __DIR__ . '/news.php';
+require __DIR__ . '/classes/DB.php';
+require_once __DIR__ . '/classes/var_dump.php';
+require __DIR__ . '/models/news.php';
 
 /*$db = new DB();
 $items = $db->query('SELECT * FROM news');*/
