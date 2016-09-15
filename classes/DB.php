1884901deabe6f645b18a7f5873c5bdf483bcaa0
Satmage <kleckers@yandex.ru>
2016-09-15 11:56:05 +0300

    Метод отправки запросов

--- classes/DB.php
+++ classes/DB.php
@@ -2,9 +2,32 @@
 
 class DB 
 {
+/*
+Подключение к БД
+*/
 	public function __construct() 
 	{
 		mysql_connect('localhost', 'root', '');
 		mysql_select_db('gallery');
+	}
+/*
+Отправка запросов в БД
+*/
+	public function query($sql) 
+	{
+		$res = mysql_query($sql);
+		
+		if(false === $res) 
+		{
+			return false;
+		}
+
+		$ret = [];
+		while ($row = mysql_fetch_object($res)) 
+		{
+			$ret[] = $row;
+		}
+
+		return $ret;
 	}
 }
