26b39715298156295a7307ced53f0dc4c35b332e
Satmage <kleckers@yandex.ru>
2016-09-15 16:49:25 +0300

    Маршрутизация

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
-require_once __DIR__ . '/classes/var_dump.php';
-require __DIR__ . '/controller/NewsController.php';
+$ctrl = isset($_GET['ctrl']) ? $_GET['ctrl'] : 'News';
 
-$controller = NewsController::actionAll();
+$act = isset($_GET['act']) ? $_GET['act'] : 'All';
+
+$controllerClassName = $ctrl . 'Controller';
+require_once __DIR__ . '/controller/' . $controllerClassName . '.php';
+$controller = new $controllerClassName;
+$method = 'action' . $act;
+$controller->$method();
+
