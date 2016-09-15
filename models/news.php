1f61496dd6e5c3d51ed7f1ddb8f0dac0c4af15cd
Satmage <kleckers@yandex.ru>
2016-09-15 16:56:43 +0300

    Правки Смена имени функции

--- models/news.php
+++ models/news.php
@@ -11,6 +11,6 @@ class News
 	public static function getAll()
 	{
 		$db = new DB();
-		return $db->query('SELECT * FROM news', 'News');
+		return $db->queryAll('SELECT * FROM news', 'News');
 	}
 } 
