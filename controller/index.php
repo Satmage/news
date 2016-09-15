b046f78f796db4bade495ce371e8540534071601
Satmage <kleckers@yandex.ru>
2016-09-15 14:18:54 +0300

    Проверяем работу Содержимое переменной $items

--- controller/index.php
+++ controller/index.php
@@ -1,9 +1,9 @@
 <?php
 
 require __DIR__ . '/../classes/DB.php';
+require_once __DIR__ . '/../classes/var_dump.php';
 
 $db = new DB();
 $items = $db->query('SELECT * FROM news');
-echo "<pre>";
-var_dump($items);
-echo "</pre>";
+wtf($items);
+
