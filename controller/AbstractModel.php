c112fd4acd4087947149cc9c01ccd79d345fb831
Satmage <kleckers@yandex.ru>
2016-09-16 14:42:26 +0300

    Перенос метода getOne() в родительский

--- controller/AbstractModel.php
+++ controller/AbstractModel.php
@@ -3,11 +3,19 @@
 abstract class AbstractModel 
 {
 
-	protected static $table = 'foobar'; 
+	protected static $table;
+	protected static $class;
 
 	public static function getAll()
 	{
 		$db = new DB();
-		return $db->queryAll('SELECT * FROM '.static::$table, 'News');
+		return $db->queryAll('SELECT * FROM '.static::$table, static::$class);
+	}
+
+	public static function getOne($id)
+	{
+		$db = new DB();
+		$sql = 'SELECT * FROM '.static::$table.' WHERE id='. $id;
+		return $db->queryOne($sql, static::$class);
 	}
 }
